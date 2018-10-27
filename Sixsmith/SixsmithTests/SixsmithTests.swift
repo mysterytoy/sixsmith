6ae2fc53d272c568622cb602bf8b301716cd917e
Edward Toy <edward.toy@pm.me>
2018-10-27 11:12:26 +0100

    Refactor to remove redundant equality tests.

--- Sixsmith/SixsmithTests/SixsmithTests.swift
+++ Sixsmith/SixsmithTests/SixsmithTests.swift
@@ -7,24 +7,10 @@ class HexOperationTests: XCTestCase {
         let x = Hex(q: -1, r: 0, s: 1)
         let y = Hex(q: -1, r: 0, s: 1)
 
-        XCTAssertEqual(x, y)
-    }
-
-    func testHexEqualityOperator() {
-        let x = Hex(q: -1, r: 0, s: 1)
-        let y = Hex(q: -1, r: 0, s: 1)
-
         XCTAssertTrue(x == y)
     }
 
     func testHexInequality() {
-        let x = Hex(q: 1, r: 2, s: 3)
-        let y = Hex(q: 3, r: 2, s: 1)
-
-        XCTAssertNotEqual(x, y)
-    }
-
-    func testHexInequalityOperator() {
         let x = Hex(q: 1, r: 2, s: 3)
         let y = Hex(q: 3, r: 2, s: 1)
 
