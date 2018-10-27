8f769f8e134122c1e4fa18dedf7049728d42522f
Edward Toy <edward.toy@pm.me>
2018-10-27 11:16:42 +0100

    Multiplication behaviour added to Hex.

--- Sixsmith/SixsmithTests/SixsmithTests.swift
+++ Sixsmith/SixsmithTests/SixsmithTests.swift
@@ -44,4 +44,18 @@ class HexOperationTests: XCTestCase {
 
         XCTAssertEqual(x - y, Hex(q: 0, r: 2, s: -12))
     }
+
+    func testPositiveHexMultiplication() {
+        let x = Hex(q: 128, r: 256, s: 1024)
+        let y = Hex(q: 2, r: 4, s: 2)
+
+        XCTAssertEqual(x * y, Hex(q: 256, r: 1024, s: 2048))
+    }
+
+    func testNegativeHexMultiplication() {
+        let x = Hex(q: -4, r: -25, s: 100)
+        let y = Hex(q: -2, r: 4, s: -10)
+
+        XCTAssertEqual(x * y, Hex(q: 8, r: -100, s: -1000))
+    }
 }
