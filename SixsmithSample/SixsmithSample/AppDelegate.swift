49b9dcf1487be081917fc5c0901bcc3f8e3d4620
Edward Toy <edward.toy@pm.me>
2019-02-06 21:57:57 +0000

    Reorder app delegate in sample app

--- SixsmithSample/SixsmithSample/AppDelegate.swift
+++ SixsmithSample/SixsmithSample/AppDelegate.swift
@@ -3,17 +3,15 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    var coordinator: AppCoordinator!
     var window: UIWindow?
+    var coordinator: AppCoordinator?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
         self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.coordinator = AppCoordinator()
 
         guard let window = self.window else { fatalError("Unable to get Window in AppDelegate") }
-
-        coordinator = AppCoordinator()
+        guard let coordinator = self.coordinator else { fatalError("Unable to initialise AppCoordinator") }
 
         window.rootViewController = coordinator.rootViewController
         window.makeKeyAndVisible()
